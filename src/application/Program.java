60123398a2e3fd5335ecfdcf25e509e338a5290a
William Carvalho <thelordl2@hotmail.com>
2021-06-24 22:18:43 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }

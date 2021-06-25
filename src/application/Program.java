fe58a8ce265cf8db20d49426fa0d8bbb06fa5cb3
William Carvalho <thelordl2@hotmail.com>
2021-06-24 22:38:01 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
